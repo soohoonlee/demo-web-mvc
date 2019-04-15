e223c08e5dabd460ff592a5dee431f4c9f37d128
soohoonlee <jjinimania@gmail.com>
2019-04-16 00:35:22 +0900

    HttpMethod Mapping

---
+++ src/test/java/me/ssoon/demowebmvc/SampleControllerTest.java
@@ -0,0 +1,35 @@
+package me.ssoon.demowebmvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class SampleControllerTest {
+    
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    public void helloTest() throws Exception {
+        mockMvc.perform(get("/hello"))
+            .andDo(print())
+            .andExpect(status().isOk());
+        mockMvc.perform(put("/hello"))
+            .andDo(print())
+            .andExpect(status().isMethodNotAllowed());
+        mockMvc.perform(post("/hello"))
+            .andDo(print())
+            .andExpect(status().isMethodNotAllowed());
+    }
+}
