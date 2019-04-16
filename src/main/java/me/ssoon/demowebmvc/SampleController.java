d392bea18109d8bf775bcc760d10ea01618025bc
soohoonlee <jjinimania@gmail.com>
2019-04-17 01:53:24 +0900

    Headers and Request Parameters

--- src/main/java/me/ssoon/demowebmvc/SampleController.java
+++ src/main/java/me/ssoon/demowebmvc/SampleController.java
@@ -1,6 +1,5 @@
 package me.ssoon.demowebmvc;
 
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,10 +7,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class SampleController {
 
-    @GetMapping(
-        value = "/hello",
-        consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
-        produces = MediaType.TEXT_PLAIN_VALUE)
+    @GetMapping(value = "/hello", params = "name=soohoon")
     @ResponseBody
     public String hello() {
         return "hello";
--- src/test/java/me/ssoon/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/ssoon/demowebmvc/SampleControllerTest.java
@@ -8,7 +8,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -22,7 +21,7 @@ public class SampleControllerTest {
     @Test
     public void helloTest() throws Exception {
         mockMvc.perform(get("/hello")
-            .contentType(MediaType.APPLICATION_JSON))
+            .param("name", "soohoon"))
             .andDo(print())
             .andExpect(status().isOk());
     }
