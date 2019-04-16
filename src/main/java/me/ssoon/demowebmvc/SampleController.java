d35806952dbb5b2f95d76a6104eadbd104e3028f
soohoonlee <jjinimania@gmail.com>
2019-04-17 01:28:28 +0900

    Consumes and Produces

--- src/main/java/me/ssoon/demowebmvc/SampleController.java
+++ src/main/java/me/ssoon/demowebmvc/SampleController.java
@@ -1,18 +1,20 @@
 package me.ssoon.demowebmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/hello")
 public class SampleController {
 
-    @GetMapping("/soohoon")
+    @GetMapping(
+        value = "/hello",
+        consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+        produces = MediaType.TEXT_PLAIN_VALUE)
     @ResponseBody
-    public String helloSoohoon() {
-        return "hello soohoon";
+    public String hello() {
+        return "hello";
     }
 
 }
