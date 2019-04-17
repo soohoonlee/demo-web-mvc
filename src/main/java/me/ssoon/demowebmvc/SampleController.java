883976bfc4aa39d356713b63ce900195d161dbc5
soohoonlee <jjinimania@gmail.com>
2019-04-18 00:34:19 +0900

    OPTIONS and HEADER

--- src/main/java/me/ssoon/demowebmvc/SampleController.java
+++ src/main/java/me/ssoon/demowebmvc/SampleController.java
@@ -2,14 +2,21 @@ package me.ssoon.demowebmvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SampleController {
 
-    @GetMapping(value = "/hello", params = "name=soohoon")
+    @GetMapping(value = "/hello")
     @ResponseBody
     public String hello() {
+        return "hello";
+    }
+
+    @PostMapping(value = "/hello")
+    @ResponseBody
+    public String helloPost() {
         return "hello";
     }
 
