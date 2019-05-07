4f57ef6f3d1112900e612990ff5a64f10d7d6c8e
soohoonlee <jjinimania@gmail.com>
2019-05-08 00:24:37 +0900

    Thymeleaf Form

--- src/test/java/me/ssoon/demowebmvc/SampleControllerTest.java
+++ src/test/java/me/ssoon/demowebmvc/SampleControllerTest.java
@@ -1,9 +1,12 @@
 package me.ssoon.demowebmvc;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,7 +23,15 @@ public class SampleControllerTest {
     MockMvc mockMvc;
 
     @Test
-    public void deleteEvent() throws Exception {
+    public void eventForm() throws Exception {
+        mockMvc.perform(get("/events/form"))
+            .andDo(print())
+            .andExpect(view().name("/events/form"))
+            .andExpect(model().attributeExists("event"));
+    }
+
+    @Test
+    public void postEvent() throws Exception {
         mockMvc.perform(post("/events")
                 .param("name", "soohoon")
                 .param("limit", "20"))
