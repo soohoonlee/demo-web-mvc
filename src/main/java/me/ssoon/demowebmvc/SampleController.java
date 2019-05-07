cf213c4e5b7df16c69a60bae69efecd6bf48b998
soohoonlee <jjinimania@gmail.com>
2019-05-08 01:17:52 +0900

    @Validated

--- src/main/java/me/ssoon/demowebmvc/SampleController.java
+++ src/main/java/me/ssoon/demowebmvc/SampleController.java
@@ -1,9 +1,9 @@
 package me.ssoon.demowebmvc;
 
-import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,12 +22,10 @@ public class SampleController {
 
     @PostMapping("/events/name/{name}")
     @ResponseBody
-    public Event getEvent(final @Valid @ModelAttribute Event event, final BindingResult bindingResult) {
+    public Event getEvent(final @Validated(Event.ValidateName.class) @ModelAttribute Event event, final BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             System.out.println("==================================");
-            bindingResult.getAllErrors().forEach(c -> {
-                System.out.println(c.toString());
-            });
+            bindingResult.getAllErrors().forEach(c -> System.out.println(c.toString()));
         }
         return event;
     }
